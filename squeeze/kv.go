64ed86a800db531921febfb46fd1a8b84a6df098
nochek <nochek07@list.ru>
2020-03-11 13:05:39 +0300

    Test kv.go

--- squeeze/kv.go
+++ squeeze/kv.go
@@ -23,17 +23,17 @@ func (kvByType KeyValueByIndex) condition(kv1, kv2 KeyValue) bool {
 	return kv1.Value < kv2.Value
 }
 
-func sortByType(kvInterface KeyValueInterface, kvArray []KeyValue) []KeyValue {
-	sort.Slice(kvArray, func(i, j int) bool {
-		return kvInterface.condition(kvArray[i], kvArray[j])
-	})
-	return kvArray
-}
-
 func (kvByType KeyValueByRepeat) sort(kvArray []KeyValue) []KeyValue {
 	return sortByType(kvByType, kvArray)
 }
 
 func (kvByType KeyValueByIndex) sort(kvArray []KeyValue) []KeyValue {
 	return sortByType(kvByType, kvArray)
+}
+
+func sortByType(kvInterface KeyValueInterface, kvArray []KeyValue) []KeyValue {
+	sort.Slice(kvArray, func(i, j int) bool {
+		return kvInterface.condition(kvArray[i], kvArray[j])
+	})
+	return kvArray
 }
